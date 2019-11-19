ad2aef2b612fbdfb965c3ff75bb90a03b7f0b1dc
YianShi <yxs600@case.com>
2019-11-19 18:40:29 -0500

    change Cost Interface

--- src/dijkstra/Cost.java
+++ src/dijkstra/Cost.java
@@ -10,13 +10,13 @@ interface Cost extends Comparable{
    boolean isKnown();
 
     //Returns a cost based on private variables only
-    LocalTime cost();
+    Cost cost();
 
     //Return a cost based on the input and private variable(s)
-    LocalTime cost(Object object);
+    Cost cost(Object object);
 
     //returns a new cost after adding the cost that corresponds with the object
-    PathTime plus(Object object);
+    Cost plus(Object object);
 
     int compareTo(Object other);
 }
