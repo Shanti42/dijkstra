4b79ebceeb6ebb8e6a40b8d3f39aee8d83c7d4cb
Shanti Polara <smp163@case.edu>
2019-11-19 19:01:52 -0500

    add connectiontest

--- src/dijkstra/ConnectionTest.java
+++ src/dijkstra/ConnectionTest.java
@@ -1,2 +1,63 @@
-package dijkstra.tests;
+package dijkstra;
+
+import org.junit.Test;
+
+import java.lang.annotation.Target;
+
+
+class ConnectionTest{
+    Connection connection;
+    Node node = Node.of("BOB", );
+
+
+    public ConnectionTest(){
+       connection = new Connection(){
+           @Override
+           boolean isLowerCost(Connection connection, Object obj) {
+               return false;
+           }
+       };
+    }
+
+    @Test
+    public void testConnection(){
+
+    }
+
+    @Test
+    public void testGetOriginID(){
+
+    }
+
+    @Test
+    public void testGetOrigin(){
+
+    }
+
+    @Test
+    public void testGetDestination(){
+
+    }
+
+    @Test
+    public void testGetCost(){
+
+    }
+
+    @Test
+    public void testGetConnectionType(){
+
+    }
+
+    @Test
+    public void testIsLowerCost(){
+
+    }
+
+    @Test
+    public void testHashCode(){
+
+    }
+
+}
 
