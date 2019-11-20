7418f9975e2254017ef0ec9fbb479b754e58e87d
YianShi <yxs600@case.com>
2019-11-19 19:01:32 -0500

    add simpleCost

--- src/dijkstra/Cost.java
+++ src/dijkstra/Cost.java
@@ -1,7 +1,6 @@
 package dijkstra;
 
 import java.math.BigInteger;
-import java.time.LocalTime;
 
 interface Cost extends Comparable{
 
@@ -10,10 +9,10 @@ interface Cost extends Comparable{
    boolean isKnown();
 
     //Returns a cost based on private variables only
-    Cost cost();
+    BigInteger cost();
 
     //Return a cost based on the input and private variable(s)
-    Cost cost(Object object);
+    BigInteger cost(Object object);
 
     //returns a new cost after adding the cost that corresponds with the object
     Cost plus(Object object);
