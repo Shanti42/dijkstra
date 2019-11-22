ee6a4754fe9f2b734428c3febf270a76ee92ca4d
Caitlin Campbell <cac226@case.edu>
2019-11-21 23:30:23 -0500

    added auto-generated equals method

--- src/dijkstra/PathNodeTest.java
+++ src/dijkstra/PathNodeTest.java
@@ -15,13 +15,16 @@ import static org.junit.Assert.assertTrue;
 public class PathNodeTest {
     private Cost<Addable> cost1 = Cost.of(SimpleAddable.of(BigInteger.valueOf(1)));
     private Cost<Addable> cost2 = Cost.of(SimpleAddable.of(BigInteger.valueOf(2)));
+    private Cost<Addable> cost3 = Cost.of(SimpleAddable.of(10));
 
     private Node node1 = Node.of("ABC", cost1);
-    private Node node2 = Node.of("BCD", cost2);
+    private Node node2 = Node.of("XYZ", cost2);
+    private Node node3 = Node.of("MNO", cost3);
 
 
     private PathNode pathNode1 = PathNode.of(node1, cost1);
     private PathNode pathNode2 = PathNode.of(node2, cost2, pathNode1);
+    private PathNode pathNode3 = PathNode.of(node3, cost3, pathNode2);
 
     @Test
     public void getNode_Test() {
@@ -35,10 +38,9 @@ public class PathNodeTest {
 
     @Test
     public void getTotalCost_Test() {
-        Cost cost_a = pathNode2.getTotalCost();
-        Cost cost_b = cost1.plus(cost2);
-        assertEquals(cost_a, cost_b);
         assertEquals(pathNode1.getTotalCost(), cost1);
+        assertEquals(pathNode2.getTotalCost(), cost1.plus(cost2));
+        assertEquals(pathNode3.getTotalCost(), cost1.plus(cost2.plus(cost3)));
     }
 
 
