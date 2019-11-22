f816bd0507b6c2ea9f38d95256328669e5cb11eb
YianShi <yxs600@case.com>
2019-11-22 09:04:09 -0500

    SimpleConnection Test

--- src/dijkstra/SimpleConnection.java
+++ src/dijkstra/SimpleConnection.java
@@ -1,5 +1,6 @@
 package dijkstra;
 
+import java.util.Objects;
 
 public final class SimpleConnection extends Connection {
 
@@ -8,12 +9,17 @@ public final class SimpleConnection extends Connection {
     }
 
     public static final SimpleConnection of(Node origin, Node destination, Cost cost, ConnectionType connectionType){
+    	Objects.requireNonNull(origin, "SimpleCollection, of -> origin null");
+    	Objects.requireNonNull(destination, "SimpleCollection, of -> destination null");
+    	Objects.requireNonNull(cost, "SimpleCollection, of -> cost null");
+    	Objects.requireNonNull(connectionType, "SimpleCollection, of -> connectionType null");
         return new SimpleConnection(origin, destination, cost, connectionType);
     }
 
 
     @Override
     boolean isLowerCost(Connection connection) {
+    	Objects.requireNonNull(connection, "SimpleCollection, isLowerCost -> connection null");
         return cost().compareTo(connection.cost()) < 0;
     }
 }
