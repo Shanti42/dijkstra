a74a8400f5179168469860480e5877ae1f783f5d
Shanti Polara <smp163@case.edu>
2019-11-21 20:57:34 -0500

    Update ConnectionGroupTest.java

--- src/dijkstra/ConnectionGroupTest.java
+++ src/dijkstra/ConnectionGroupTest.java
@@ -23,10 +23,15 @@ public class ConnectionGroupTest {
     public static Cost cost2 = Cost.of(SimpleAddable.of(BigInteger.valueOf(2)));
     public static Cost cost3 = Cost.of(SimpleAddable.of(BigInteger.valueOf(3)));
     public static Cost cost4 = Cost.of(SimpleAddable.of(BigInteger.valueOf(4)));
+    public static Cost costBig = Cost.of(SimpleAddable.of(BigInteger.valueOf(10)));
     Connection nullConnection = null;
 
     public static Connection connection;
 
+
+
+    Node origin = Node.of("BOB", new SimpleCost(BigInteger.valueOf(0)));
+    ConnectionGroup connectionGroup = ConnectionGroup.of(origin);
 
     public static ConnectionType busConnectionType = ConnectionType.of("BUS");
     public static ConnectionType trainConnectionType = ConnectionType.of("TRAIN");
@@ -52,8 +57,8 @@ public class ConnectionGroupTest {
         connection = SimpleConnection.of(origin, destination, cost3, busConnectionType);
 
 
-        connectionLarge = SimpleConnection.of(origin, destination, cost3, busConnectionType);
-        connectionSmall = SimpleConnection.of(origin, destination, cost3, busConnectionType);
+        connectionLarge = SimpleConnection.of(origin, destination, cost4, busConnectionType);
+        connectionSmall = SimpleConnection.of(origin, destination, cost2, busConnectionType);
         badOriginConnection = SimpleConnection.of(other, destination, cost3, busConnectionType);
     }
 
@@ -95,12 +100,12 @@ public class ConnectionGroupTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void testConnectionGroupNullConnect() {
+    public void testConnectionGroupAddNullConnect() {
         connectionGroup.add(nullConnection);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testConnectionGroupBadOrigin() {
+    public void testConnectionGroupAddBadOrigin() {
         connectionGroup.add(badOriginConnection);
     }
 
@@ -112,31 +117,31 @@ public class ConnectionGroupTest {
     @Test
     void testConnectionGroupRemove() {
 
-        Connection nullConnection = null;
-        //Connection intentionally created with wrong origin for testing purposes
-        Connection badOriginConnection = SimpleConnection.of("Code", Leg.of(Node.of("JFK", Duration.ofHours(3)), destination), connectionSchedule, config1);
-
         ConnectionGroup connectionGroup = ConnectionGroup.of(origin);
 
-        assertThrows(NullPointerException.class, () -> {
-            connectionGroup.add(nullConnection);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            connectionGroup.add(badOriginConnection);
-        });
 
         connectionGroup.add(connection);
-        connectionGroup.add(connection1);
 
         assertTrue(connectionGroup.remove(connection));
         assertFalse(connectionGroup.remove(connection));
 
         Set<Connection> connections = new HashSet<>();
-        connections.add(connection1);
+        connections.add(connection);
 
-        assertEquals(connections, connectionGroup.connectionsAtOrAfter(connection1.departureTime()));
+        assertEquals(connections, connectionGroup.connectionsAtOrAfter(cost2));
 
     }
+
+    @Test(expected = NullPointerException.class)
+    public void testConnectionGroupRemoveNullConnect() {
+        connectionGroup.remove(nullConnection);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectionGroupRemoveBadOrigin() {
+        connectionGroup.remove(badOriginConnection);
+    }
+
 
     /**
      * ConnectionGroup connectionsAtOrAfter() test
@@ -144,75 +149,23 @@ public class ConnectionGroupTest {
     @Test
     void testConnectionGroupConnectionsAtOrAfter() {
 
-        ConnectionGroup connectionGroup = ConnectionGroup.of(Node.of("CLE", Duration.ofHours(1)));
+        ConnectionGroup connectionGroup = ConnectionGroup.of(origin);
 
         connectionGroup.add(connection);
-        connectionGroup.add(connection1);
-        connectionGroup.add(connection2);
-        assertNotNull(connectionGroup.connectionsAtOrAfter(connection.departureTime()));
+        connectionGroup.add(connectionSmall);
+        connectionGroup.add(connectionLarge);
+        assertNotNull(connectionGroup.connectionsAtOrAfter(cost2);
 
         Set<Connection> connections = new HashSet<>();
         connections.add(connection);
-        connections.add(connection1);
-        connections.add(connection2);
-        assertEquals(connections, connectionGroup.connectionsAtOrAfter(connection.departureTime()));
+        connections.add(connectionSmall);
+        connections.add(connectionLarge);
+        assertEquals(connections, connectionGroup.connectionsAtOrAfter(cost3));
 
-        assertEquals(new HashSet<>(), connectionGroup.connectionsAtOrAfter(LocalTime.of(20, 0)));
-    }
-
-    /**
-     *      ---- ConnectionSchedule tests ---
-     */
-    /**
-     * ConnectionSchedule build method test
-     */
-    @Test
-    void testConnectionScheduleOf() {
-
-        assertThrows(NullPointerException.class, () -> {
-            ConnectionSchedule.of(null, null);
-        });
-        assertThrows(NullPointerException.class, () -> {
-            ConnectionSchedule.of(depart, null);
-        });
-        assertThrows(NullPointerException.class, () -> {
-            ConnectionSchedule.of(null, arrival);
-        });
-
-        assertNotNull(ConnectionSchedule.of(depart, arrival));
-    }
-
-    /**
-     * ConnectionSchedule isLowerCost() test
-     */
-    @Test
-    void testConnectionScheduleIsShort() {
-
-        Duration oneHour = Duration.ofHours(1);
-        Duration twoHour = Duration.ofHours(2);
-        Duration min30 = Duration.ofMinutes(30);
-
-        LocalTime oneHourLoc = LocalTime.of(1, 0);
-        LocalTime min30Loc = LocalTime.of(0, 30);
-        LocalTime twoHourLoc = LocalTime.of(2, 0);
-
-        ConnectionSchedule hourConnection = ConnectionSchedule.of(oneHourLoc, twoHourLoc);
-
-        assertThrows(NullPointerException.class, () -> {
-            hourConnection.isShort(null);
-        });
-        assertTrue(hourConnection.isShort(oneHour), "Duration equal to connection");
-        assertFalse(hourConnection.isShort(min30), "Duration shorter than connection");
-        assertTrue(hourConnection.isShort(twoHour), "Duration longer than connection");
+        assertEquals(new HashSet<>(), connectionGroup.connectionsAtOrAfter(costBig));
     }
 
 
-    Node origin = Node.of("BOB", new SimpleCost(BigInteger.valueOf(0)));
-    ConnectionGroup connectionGroup = ConnectionGroup.of(origin);
-
-    public ConnectionGroupTest() {
-
-    }
 
     @Test
     public void testOf() {
@@ -223,21 +176,10 @@ public class ConnectionGroupTest {
     public void testOfNullVal() {
         ConnectionGroup.of(null);
     }
-
-
+    
     @Test
-    public void testAdd() {
-
-    }
-
-    @Test
-    public void testRemove() {
-
-    }
-
-    @Test
-    public void testConnectionsAtOrAfter() {
-
+    public void testGetOrigin() {
+        assertEquals(connectionGroup.getOrigin(), origin);
     }
 
     @Test
@@ -245,14 +187,4 @@ public class ConnectionGroupTest {
 
     }
 
-
-    @Test
-    public void testGetOrigin() {
-
-    }
-
-    @Test
-    public void testValidateConnectionOrigin() {
-
-    }
 }
